75476adcd1e43390792d3bfa9f9b226d741bab14
MUZAAQI <muzaaqi@yahoo.com>
2025-12-04 14:45:36 +0700

    chore(ui): Revamp Home page layout and styling
    Updated the Home page with a new background image, improved layout using flex and grid, and enhanced typography. Added a branded header, subtitle, and a 'Get Started' button for better user engagement.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="min-h-svh bg-background flex items-center justify-center">
-      <div>
-        <h1 className="text-primary">TODO APP</h1>
+    <div className="flex min-h-svh items-center justify-center bg-[url('/background.svg')] bg-cover">
+      <div className="flex flex-col items-center justify-center gap-6 text-center">
+        <div className="grid justify-items-center">
+          <span className="bg-primary w-fit px-4 py-1 rounded-full text-primary-foreground">mudo.muzaaqi.my.id</span>
+          <h1 className="text-primary font-sans text-9xl font-bold">MUDO</h1>
+          <h2 className="text-secondary-foreground text-2xl tracking-widest">
+            record your tasks, effortlessly
+          </h2>
+        </div>
+        <Button className="w-fit">Get Started</Button>
       </div>
     </div>
   );
